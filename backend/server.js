f3f64dc6e15fc6c2d8148603b5e91c026e282f5c
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-02-03 09:04:02 +0530

    setup mongoose connection

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 import app from "./src/app.js";
+import connectDB from "./src/config/db.js";
 import { config } from "./src/config/envConfig.js";
 
 const startServer = () => {
   const PORT = config.port || 3000;
 
-  app.listen(PORT, () => {
+  app.listen(PORT, async () => {
     console.log(`Server is running on http://localhost:${PORT}`);
+    await connectDB();
   });
 };
 
--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,8 @@
 import express from "express";
+import authRoutes from "./routes/auth.route.js";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello welcome to Ecommerce Store</h1>");
-});
+app.use("/api/auth", authRoutes);
 
 export default app;
