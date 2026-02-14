c9f093179d758536ef5408c1a0b19549e3f38d7f
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-02-14 13:03:16 +0530

    implement login with password verification and JWT

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -2,6 +2,8 @@ import { asyncHandler } from "../middlewares/asyncHandler.js";
 import createHttpError from "http-errors";
 import { User } from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { config } from "../config/envConfig.js";
 
 export const registerUser = asyncHandler(async (req, res, next) => {
   const { name, email, password } = req.body;
@@ -12,14 +14,44 @@ export const registerUser = asyncHandler(async (req, res, next) => {
 
   const existUser = await User.findOne({ email });
   if (existUser) {
-    return next(createHttpError(400, "User already registered"));
+    return next(createHttpError(400, "Email already registered"));
   }
 
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  const newUser = await User.create({ name, email, password: hashedPassword });
+  await User.create({ name, email, password: hashedPassword });
 
   res
     .status(201)
-    .json({ success: true, message: "User registered successfully", newUser });
+    .json({ success: true, message: "User registered successfully" });
+});
+
+export const loginUser = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(createHttpError(400, "all fields are required"));
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    return next(createHttpError(404, "User not found"));
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    return next(createHttpError(401, "Invalid email or password"));
+  }
+
+  const token = jwt.sign({ id: user._id }, config.jwtSecret, {
+    expiresIn: "7d",
+  });
+
+  res.status(200).json({
+    success: true,
+    message: "User logged in successfully",
+    user: { id: user._id, name: user.name, email: user.email },
+    token,
+  });
 });
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { registerUser } from "../controllers/auth.controller.js";
+import { loginUser, registerUser } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/signup", registerUser);
+router.post("/login", loginUser);
 
 export default router;
